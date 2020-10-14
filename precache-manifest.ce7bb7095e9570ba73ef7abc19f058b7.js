74272179f646f6eb0b9b1b6c54c687272a498bfc
ibrahim-haredy <ibrahim.haredy@vodafone.com>
2020-10-14 15:09:42 +0200

    Updates

--- precache-manifest.ce7bb7095e9570ba73ef7abc19f058b7.js
+++ precache-manifest.ce7bb7095e9570ba73ef7abc19f058b7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4ef91d9245357f88499e6466864ae3ad",
+    "revision": "fd1ecc0f800bc66c87131c1d8bcca027",
     "url": "/summit/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/summit/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "aba6a755d368a7fe4c9f",
+    "revision": "a0141182f44b92da2beb",
     "url": "/summit/static/css/main.e3d371e1.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/summit/static/js/2.5d955409.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aba6a755d368a7fe4c9f",
-    "url": "/summit/static/js/main.05d9a0ac.chunk.js"
+    "revision": "a0141182f44b92da2beb",
+    "url": "/summit/static/js/main.4bbe1389.chunk.js"
   },
   {
     "revision": "e1c5f1c98b33cbbb9bcf",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/summit/precache-manifest.22ebd4fa8a04dd17f8bd0f84dc869b6f.js"
+  "/summit/precache-manifest.ce7bb7095e9570ba73ef7abc19f058b7.js"
 );
 
 self.addEventListener('message', (event) => {
