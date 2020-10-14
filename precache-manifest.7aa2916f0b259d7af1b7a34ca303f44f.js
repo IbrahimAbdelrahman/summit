029343c4cde4494782d4b0388e038b3bd08a0c87
ibrahim-haredy <ibrahim.haredy@vodafone.com>
2020-10-14 14:25:31 +0200

    Updates

---
+++ precache-manifest.7aa2916f0b259d7af1b7a34ca303f44f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ad2012ca2f534ee7ad7f68b6f55870",
+    "url": "/summit/index.html"
+  },
+  {
+    "revision": "d303ec3ba7e0028be9ec",
+    "url": "/summit/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "74c3b6277ec8a9e9b72e",
+    "url": "/summit/static/css/main.e3d371e1.chunk.css"
+  },
+  {
+    "revision": "d303ec3ba7e0028be9ec",
+    "url": "/summit/static/js/2.1cd1d2af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/summit/static/js/2.1cd1d2af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74c3b6277ec8a9e9b72e",
+    "url": "/summit/static/js/main.6619b0b5.chunk.js"
+  },
+  {
+    "revision": "e1c5f1c98b33cbbb9bcf",
+    "url": "/summit/static/js/runtime-main.2f37e7ec.js"
+  }
+]);
